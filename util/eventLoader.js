7641d07b6e6aff0751944ec796d3a1433bcbd898
AirFusion45 <jfang.cv.ca.us@gmail.com>
2017-07-11 18:43:45 -0700

    util stuff

---
+++ util/eventLoader.js
@@ -0,0 +1,9 @@
+const reqEvent = (event) => require (`../events/${event}`)
+module.exports = client => {
+    client.on('ready', () => reqEvent('ready')(client));
+    client.on('reconnecting', () => reqEvent('reconnecting')(client));
+    client.on('disconnect', () => reqEvent('disconnect')(client));
+    client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
+    client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
+    client.on('message', reqEvent('message'));
+};
