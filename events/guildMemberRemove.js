def479b572edac4efe5139418c6219f9701cee4b
AirFusion45 <jfang.cv.ca.us@gmail.com>
2017-07-30 00:35:13 -0700

    stopped multi tagging | more dtils in changelog
    stopped multi tagging user when they leave server

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,12 @@
+module.exports = member => {
+     let guild = member.guild;
+    if (member.user.bot) {
+        guild.defaultChannel.sendMessage(`Goodbye to Bot ${member.user} :( `);
+
+    }
+    else {
+        guild.defaultChannel.sendMessage(`Goodbye to user ${member.user} :(`);
+    }
+
+}
+
