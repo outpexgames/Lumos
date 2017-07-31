ccd388dfc945187400feea13ad2b2409f9e980c4
AirFusion45 <jfang.cv.ca.us@gmail.com>
2017-07-31 01:46:08 -0700

    !!!CRITICAL UPDATE | View changelog for more dtils

--- commands/dndexit.js
+++ commands/dndexit.js
@@ -1,6 +1,6 @@
  exports.run = function (client, message, args, args2, cmd, config) {
   let role = message.guild.roles.find("name", "DND/Do Not Disturb");
         let member = message.guild.member(message.author);
-        message.reply(`${member} has exited Do Not Disturb mode!`);
+        message.channel.send(`${member} has exited Do Not Disturb mode!`);
         member.removeRole(role).catch(console.error);
  };
