5a17d5bccb9afea71697e43a13feb3a704079892
AirFusion45 <jfang.cv.ca.us@gmail.com>
2017-07-31 01:39:35 -0700

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = member => {
      let guild = member.guild;
     if (member.user.bot) {
-        guild.defaultChannel.sendMessage(`A Wild Bot Has Appeared On The Server... \n The Bot's Name Is: ${member.user} OHHHHHHH... :/`)
+        guild.defaultChannel.send(`A Wild Bot Has Appeared On The Server... \n The Bot's Name Is: ${member.user} OHHHHHHH... :/`)
     }
     else {
-        guild.defaultChannel.sendMessage(`Welcome ${member.user} to CMS Chat!`)
+        guild.defaultChannel.send(`Welcome ${member.user} to CMS Chat!`)
     }
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,11 +1,11 @@
 module.exports = member => {
      let guild = member.guild;
     if (member.user.bot) {
-        guild.defaultChannel.sendMessage(`Goodbye to Bot ${member.user} :( `);
+        guild.defaultChannel.send(`Goodbye to Bot ${member.user} :( `);
 
     }
     else {
-        guild.defaultChannel.sendMessage(`Goodbye to user ${member.user} :(`);
+        guild.defaultChannel.send(`Goodbye to user ${member.user} :(`);
     }
 
 }
