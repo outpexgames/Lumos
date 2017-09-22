c0c85075b427d29b480a1376c6c8b8d31c85c3e7
AirFusion <jfang.cv.ca.us@gmail.com>
2017-09-21 21:40:42 -0700

    Major update! | View changelog for more dtils
    Sorry for the late update... :/ This was a major update, and I recently was really busy... :( But its out! :D and I can tell everyone that the public release is coming out soon! :)

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = member => {
      let guild = member.guild;
     if (member.user.bot) {
-        guild.defaultChannel.send(`A Wild Bot Has Appeared On The Server... \n The Bot's Name Is: ${member.user} OHHHHHHH... :/`)
+        guild.channels.find("name", "general").send(`A Wild Bot Has Appeared On The Server... \n The Bot's Name Is: ${member.user} OHHHHHHH... :/`)
     }
     else {
-        guild.defaultChannel.send(`Welcome ${member.user} to CMS Chat!`)
+        guild.channels.find("name", "general").send(`Welcome ${member.user} to ${guild.name}`)
     }
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,11 +1,12 @@
 module.exports = member => {
      let guild = member.guild;
     if (member.user.bot) {
-        guild.defaultChannel.send(`Goodbye to Bot ${member.user} :( `);
+       // guild.defaultChannel.send(`Goodbye to Bot ${member.user} :( `);
+       guild.channels.find("name", "general").send(`Goodbye to Bot ${member.user} :( `)
 
     }
     else {
-        guild.defaultChannel.send(`Goodbye to user ${member.user} :(`);
+        guild.channels.find("name", "general").send(`Goodbye to user ${member.user} :(`);
     }
 
 }
