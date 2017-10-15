215dc523ce99ca77b0d3e511ac71631478ae783e
AirFusion <jfang.cv.ca.us@gmail.com>
2017-10-15 10:23:48 -0700

    Fixed & added
    Added: Getip & Iplookup Fixed: Masspurge ---- view changelog for more details

--- commands/masspurge.js
+++ commands/masspurge.js
@@ -7,7 +7,7 @@ exports.run = function (client, message, args, args2, cmd1) {
         .addField("**Usage:**", `${config.prefix}masspurge <x page>`)
         .addField("**Example:**", `${config.prefix}masspurge 1`)
         .addField("**Expected Result From Example:**", "1 page of messages should be purged from current channel.")
-        if (args2.join(' ') == "") return message.channel.send({embed: embed1})
+        if (args.join(' ')=="") return message.channel.send({embed: embed1})
     if (message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')) {
         let messagecount = parseInt(args2.join(' '));
         message.channel.fetchMessages({ limit: messagecount }).then(messages => message.channel.bulkDelete(messages));
