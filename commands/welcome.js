748618167b4d902976e58dab793b1c86dfd2dacb
Ethan Mitchell <ethamitc@gmail.com>
2018-11-13 10:45:10 -0500

    Create welcome.js

---
+++ commands/welcome.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+  if(!message.member.hasPermission('MANAGE_GUILD')) return message.reply('You don't have the permissions required to access this command!');
+  if(!message.mentions.channels.first) return message.reply('Please mention a channel! (Usage: [Prefix]welcome [#channel])');
+  client.guildMemberEnmap.set(message.guild.id, message.mentions.channels.first.id);
+  message.reply('Successfully set the welcome channel!');
+}
